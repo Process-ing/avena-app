912ffd516db654fdf8a75cad21004fa53f809d8b
toino <me@toino.pt>
2026-02-14 15:53:47 +0000

    Use providers in cookbook screen

---
+++ lib/provider/cook_book.dart
@@ -0,0 +1,33 @@
+import 'package:avena/model/recipe.dart';
+import 'package:avena/provider/recipe.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'cook_book.g.dart';
+
+@riverpod
+class CookBookCategoryNotifier extends _$CookBookCategoryNotifier {
+  String? build() => null;
+
+  void toggle(String category) {
+    state = category == state ? null : category;
+  }
+}
+
+@riverpod
+class CookBookPantryNotifier extends _$CookBookPantryNotifier {
+  bool build() => true;
+
+  void toggle() {
+    state = !state;
+  }
+}
+
+@riverpod
+Future<List<Recipe>> cookBookRecipes(Ref ref) {
+  final category = ref.watch(cookBookCategoryProvider);
+  final pantry = ref.watch(cookBookPantryProvider);
+
+  return ref.watch(
+    filteredRecipesProvider(category: category, pantry: pantry).future,
+  );
+}
